4b8c2a815669ee937f2a878637bfb23a6c11716d
annaprotasio <avp25@cam.ac.uk>
2020-08-26 12:25:43 +0100

    Update make_fasta_from_blast_result.sh
    update script - added functionality

--- avp25_scripts/make_fasta_from_blast_result.sh
+++ avp25_scripts/make_fasta_from_blast_result.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-
 if [ $# -ne 4 ]
 then
-    echo -e "\nusage: $0 <genome.fa> <fasta.in> <min_length> <flank>\n"
-    echo -e "DESCRIPTION: This script takes a fasta sequence <fasta.in>, blasts it to the <genome.fa>, recovers locations with alingment lenght > <min_length>, prints them as bed file, extends bed coordinates <flank> bases in each direction,  and makes fasta from that BED."
-    echo -e "OUTPUT    :  produces a <fasta.in.bed> which is the blast results BED file file"
-    echo -e "          :  produces a <fasta.in.blast.flank.bed> which is the extended BED"
-    echo -e "          :  produces a <fasta.in.blast.flank.bed.fa> bedtools getfasta \n"
-    echo -e "REQUIRES  :  file with chromosome lengths with same name as genome, as in <genome.fa.length>, and found in the same dir as genome (use \"samtools faixd\" and then \"cut -f 1,2\""
-    echo -e "          :  <genome.fa> must be formatted for blast with \"makeblastdb\" "		
+    echo -e "\nusage: $0 <genome> <fasta.in> <min_length> <flank>\n"
+    echo -e "DESCRIPTION: This script takes a fasta sequence <fasta.in>, blasts it to the genome, recovers locations with alingment length > <min_length>, prints them as bed file, extends bed coordinates <flank> bases in each direction, and makes fasta from that BED.\n"
+
+    echo -e "INPUT:       <fasta.in>    query sequence if fasta format"
+    echo -e "             <min_length>  min length of the blast hit. If set to 0, min length = (length of query)/2"
+    echo -e "             <flank>       number of bases to extend the genome coordinates of the matched locus\n"
+    
+    echo -e "OUTPUT:      produces a <fasta.in.bed> which is the blast results BED file file"
+    echo -e "             produces a <fasta.in.blast.flank.bed> which is the extended BED"
+    echo -e "             produces a <fasta.in.blast.flank.bed.fa> bedtools getfasta \n"
      
     exit
 fi
@@ -20,11 +22,23 @@ out=`basename $2`
 min_length=$3
 flank=$4
 
-blastn -query $fasta_in -db $genome -outfmt "6 qseqid sseqid pident length mismatch qstart qend sstart send sstrand" -evalue 0.0000000000000000000000000001 > $out.blast.o
+if [ $3 == 0 ]
+then 
+	min_length=`grep -v ">" $2 | wc | awk '{print $3/2}'`
+else
+	min_length=$3
+fi
 
-awk -v "ml=$min_length" '{OFS="\t"; if ($4 > ml ) { if ($10~/plus/) {print $2, $8, $9, $1, $3, "+"} else {print $2, $9, $8, $1, $3, "-"}}}' < $out.blast.o > $out.blast.bed
+echo "query sequence" $out
+echo "minimum length of blast hit = " $min_length
+echo "the hit locus will be extended" = $flank "bases in each direction"
 
-bedtools slop -s  -i $out.blast.bed -g $genome.length -b $flank > $out.blast.flank.bed
+echo "#qseqid sseqid pident length mismatch qstart qend sstart send sstrand" > $out.blast.o
+blastn -query $fasta_in -db $genome -outfmt "6 qseqid sseqid pident length mismatch qstart qend sstart send sstrand" -evalue 0.000000000000000000001 | awk -v "ml=$min_length" '{OFS="\t"; if ($4 > ml) {print $0}}' >> $out.blast.o
 
-bedtools getfasta -fi $genome -fo $out.blast.bed.fa  -bed $out.blast.flank.bed -s 
+awk '{OFS="\t"; if ($1~/^\#/) {} else { if ($10~/plus/) {print $2, $8, $9, $1, $3, "+"} else {print $2, $9, $8, $1, $3, "-"}}}' < $out.blast.o > $out.blast.bed
+
+bedtools slop -s  -i $out.blast.bed  -g /Users/ap6/Documents/Results/Repeat_analysis/v8/Ref/schisto_hic.curated.final.v8.fa.length -b $flank > $out.blast.flank.bed
+
+bedtools getfasta -fi  /Users/ap6/Documents/Results/Repeat_analysis/v8/Ref/schisto_hic.curated.final.v8.fa -fo $out.blast.bed.fa  -bed $out.blast.flank.bed -s 
 
