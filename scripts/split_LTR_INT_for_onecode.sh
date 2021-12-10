cd0a8166178ecdbfb6140fc6f7f992796802cf0d
Anna V. Protasio <avp25@cam.ac.uk>
2021-12-10 15:29:16 +0000

    update split_LTR_INT_for_onecode.sh
    Fixed coordinates from blast match as to be more flexible in finding the LTRs

--- scripts/split_LTR_INT_for_onecode.sh
+++ scripts/split_LTR_INT_for_onecode.sh
@@ -5,7 +5,7 @@ then
     echo -e "\nusage: $0 <LTR.fa> \n"
     echo -e "DESCRIPTION: 	This script uses Blast to find the LTR subparts. Blast needs to be previously installed and found in the path\n"
 
-    echo -e "DEPENDENCIES: 	Requires installation of fastq-tools \"https://anaconda.org/bioconda/blast\" \n"
+    echo -e "DEPENDENCIES: 	Requires installation of BLAST+ \"https://anaconda.org/bioconda/blast\" \n"
     echo -e "INPUT:       	<LTR.fa>    		A FASTA file with the LTR elements sequences"
     echo -e "OUTPUT:	    writes a GFF to output\n"     
 
@@ -42,18 +42,18 @@ unique=$(awk '{print $1}' < blast.$current_time | sort -u | wc -l)
 
 if [ $entries != $unique ]
 then
-    echo -e "\nMore than 1 possible output for at least one entry or one entry missing from output, please check output manually"
+    echo -e "\nWARNING: More than 1 possible output for at least one entry or one entry missing from output, please check output manually"
 fi    
 
 # parse blast 
 echo "###gff-annotation" > LTR_annotation.gff
-cat blast.$current_time | awk '{OFS="\t";if ( ($1==$2) &&  ($4<1000) && ($7<10) && ($9>1000) && ($10==$13) ) {print $1,"manual","LTR",$7,$8,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"LTR\"; Subpart \"5LTR\";\n"$1,"manual","LTR",$8+1,$9-1,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"INT\"; Subpart \"INT\";\n"$1,"manual","LTR",$9,$10,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"LTR\"; Subpart \"3LTR\";"}}'>> LTR_annotation.gff
+cat blast.$current_time | awk '{OFS="\t";if ( ($1==$2) &&  ($4<1000) && ($7<20) && ($9>1000) && ($10 >= ($13-10))) {print $1,"manual","LTR",1,$8,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"LTR\"; Subpart \"5LTR\";\n"$1,"manual","LTR",$8+1,$9-1,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"INT\"; Subpart \"INT\";\n"$1,"manual","LTR",$9,$10,".","+",".","Superfamily \""$1 "\"; Family \""$1"\"; Type \"LTR\"; Subpart \"3LTR\";"}}'>> LTR_annotation.gff
 
 # make bed file
 
 cat LTR_annotation.gff | awk '{OFS="\t"; print $1,$4-1,$5,$1"-"$NF,$6,$7}' |  tr -d '";' | sed '/^#/d' > LTR_annotation.bed
 
-rm blast.$current_time
+#rm blast.$current_time
 
 
 
